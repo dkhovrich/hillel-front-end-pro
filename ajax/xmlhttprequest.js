e1fd61fff9df20ae1c2f5fb42c2e3c5416fe84f0
Dmitriy Khovrich <dkhovrich@readdle.com>
2020-04-23 21:55:49 +0300

    get

---
+++ ajax/xmlhttprequest.js
@@ -0,0 +1,30 @@
+const xhr = new XMLHttpRequest();
+
+// let url = new URL('https://google.com/search');
+// url.searchParams.set('q', 'test me!');
+
+xhr.open("GET", "https://rickandmortyapi.com/api/character/");
+
+xhr.responseType = 'json';
+
+xhr.onload = function () {
+    if (xhr.status !== 200) {
+        console.error(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+        console.log(xhr.response);
+    }
+}
+
+xhr.onprogress = function (event) {
+    if (event.lengthComputable) {
+        console.log(`Получено ${event.loaded} из ${event.total} байт`);
+    } else {
+        console.log(`Получено ${event.loaded} байт`);
+    }
+};
+
+xhr.onerror = function () {
+    console.error("Запрос не удался");
+};
+
+xhr.send();
